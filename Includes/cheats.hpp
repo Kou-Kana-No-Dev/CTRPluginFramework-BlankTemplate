f5bf82cfb22beba9e42f08d5148e19a82f84823d
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-05-03 22:52:22 +0900

    Add files via upload

--- Includes/cheats.hpp
+++ Includes/cheats.hpp
@@ -7,12 +7,12 @@
 
 namespace CTRPluginFramework
 {
-   void km(MenuEntry *entry)
-   void fov(MenuEntry *entry)
-   void eff(MenuEntry *entry)
-   void uspeed(MenuEntry *entry)
-   void rpff(MenuEntry *entry)
-   void rprnd(MenuEntry *entry)
-   void amib(MenuEntry *entry)
+   void km(MenuEntry *entry);
+   void fov(MenuEntry *entry);
+   void eff(MenuEntry *entry);
+   void uspeed(MenuEntry *entry);
+   void rpff(MenuEntry *entry);
+   void rprnd(MenuEntry *entry);
+   void amib(MenuEntry *entry);
 }
 #endif
