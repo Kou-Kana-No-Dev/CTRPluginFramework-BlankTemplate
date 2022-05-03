3001ca328dfe71b824c07726eea93ed9750ffe97
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-05-03 22:27:44 +0900

    Add files via upload

--- Includes/cheats.hpp
+++ Includes/cheats.hpp
@@ -7,7 +7,12 @@
 
 namespace CTRPluginFramework
 {
-    using StringVector = std::vector<std::string>;
-
+   void km(MenuEntry *entry)
+   void Fov(MenuEntry *entry)
+   void eff(MenuEntry *entry)
+   void uspeed(MenuEntry *entry)
+   void rpff(MenuEntry *entry)
+   void rprnd(MenuEntry *entry)
+   void amib(MenuEntry *entry)
 }
 #endif
