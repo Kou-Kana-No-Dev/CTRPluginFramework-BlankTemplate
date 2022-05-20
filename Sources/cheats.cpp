3970dc06edbcd7be864f5c499ca7f95ebd15d83c
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-05-20 17:39:53 +0900

    Add files via upload

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -1,11 +1,8 @@
-#include "cheats.hpp"
-u32 offset = 0;
-u32 data32 = 0;
-u8 data8 = 0;
-u16 data16 = 0;
-u32 patch_address = 0;
-u16 cmp16 = 0;
-u32 cmp32 = 0;
+u32 offset = 0, offset2 = 0, data32 = 0, cmp32 = 0;
+u16 data16 = 0, cmp16 = 0;
+u8 data8 = 0, cmp8 = 0;
+float dataf = 0;
+
 
 namespace CTRPluginFramework
 {
