24ae2219b6e0761984a1ec0d6c75fd60b83a784d
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-05-27 04:47:37 +0900

    Update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -187,7 +187,7 @@ MenuFolder *Status = new MenuFolder(mving + "ステータス");
         *zksi += new MenuEntry(Shyyy + "全属性", AllAttributes, "");
 }
     *Status += zksi;
-    *Status += new MenuEntry("ファストイート", FastEat, "肉、笛、調合薬などを速く使用できます");
+    *Status += new MenuEntry(Shyyy + "ファストイート", FastEat, "肉、笛、調合薬などを速く使用できます");
 	*Status += new MenuEntry(Shyyy + "スキルスロット埋める", FillSkillSlots, "→:1番上のスキル↑:真ん中のスキル←:1番下のスキル");
 	*Status += new MenuEntry(Shyyy + "透明", Transparent, "テスト段階、使用不可");
 	*Status += new MenuEntry(Shyyy + "攻撃力1", AttackPower1, "");
