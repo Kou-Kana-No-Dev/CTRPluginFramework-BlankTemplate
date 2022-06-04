d753aa7148a7f2eadeaaafa980829fc10783078f
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-06-05 00:02:39 +0900

    Add files via upload

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -1,13 +1,24 @@
 #include "cheats.hpp"
-#include "Helpers.hpp"
 
-namespace CTRPluginFramework {
-void ludora(MenuEntry*) {
+u32 offset = 0, offset2 = 0, data32 = 0, cmp32 = 0;
+u16 data16 = 0, cmp16 = 0;
+u8 data8 = 0, cmp8 = 0;
+float dataf = 0;
+
+namespace CTRPluginFramework
+{
+void ludora(MenuEntry *entry) {
   std::string ludoralog = "Start ludora";
   std::string ludorax;
-  if (!GetInput(ludorax, ludoralog + "\n>>")) {
-    return;
-  }
-  ludoralog += ("\n" + ludorax);
+  while (ludorax != "lend") {
+if (!GetInput ( std::string ludorax, std::string ludoralog + "\n>>" )) { 
+return;
+ }
+ludoralog += ( "\n" + ludorax);
+if (ludorax == sudo) {
+return;
 }
-}  // namespace CTRPluginFramework
+ludoralog += ("\n" + ludorax + "\nsudo Success")
+}
+}
+}
