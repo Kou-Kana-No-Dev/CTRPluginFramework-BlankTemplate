53e0b95b454fbdf3ea7e1f0f13fd98885fb47b68
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-06-05 08:37:47 +0900

    Add files via upload

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -10,19 +10,32 @@ void ludora(MenuEntry* entry) {
   std::string ludoralog = "Start ludora";
   std::string ludorax;
   u16 hatehr;
+  u16 hateatk;
+  u16 hatedfc;
   while (ludorax != "lend") {
     if (!GetInput(ludorax, ludoralog + "\n>>")) {
       return;
     }
     ludoralog += ("\n" + ludorax);
-    if (ludorax == "sudo") {
-      ludoralog += ("\nsudo Success");
+    if (ludorax == "atk") {
+      ludoralog += ("\nATK >> please enter");
+      if (!GetInput(hatehr, ludoralog + "\n>>")) {
+      return;
+      }
+      	Process::Write16(0x0831B450 , hateatk);
+        Process::Write16(0x008F1C0C , hateatk);
     }
         if (ludorax == "hr") {
           ludoralog += ("\nHR >> please enter");
           if (!GetInput(hatehr, ludoralog + "\n>>")) {
-          Process::Write16(0x831B76A , hatehr);
           }
+        Process::Write16(0x0831B76A , hatehr);
+    }
+    if (ludorax == "dfc") {
+      ludoralog += ("\nDFC >> please enter");
+      if (!GetInput(hatedfc, ludoralog + "\n>>")) {
+      }
+      Process::Write16(0x0831B45E , hatedfc);
     }
   }
 }
