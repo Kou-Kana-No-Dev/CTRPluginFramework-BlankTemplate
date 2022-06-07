06fa0737ef3b7000665647ea6396a4d15ee123e2
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-06-07 20:19:04 +0900

    Add files via upload

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -33,18 +33,8 @@ void ludora(MenuEntry* entry) {
   if (slct == 3) {
     str2 = "movement";
   }
-  if(Controller::IsKeyPressed(DPadDown)) {
-    if (Controller::IsKeyPressed(L)) {
-      if (Controller::IsKeyPressed(R)) {
-        if (isopned == 1) {
-         isopned = 0;
-        }else{
-          isopned = 1;
-        }
-      }
-    }
-  }
   tp_screen.Draw(str2, x, y+20, foreground, background);
+  if(Controller::IsKeyPressed(L))
   while (isopned == 1) {
   	if(Controller::IsKeyPressed(DPadDown)) {
   	  slct=slct-1;
