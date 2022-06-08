92edbfc478ab96e6301eb01dfa89542fabaa29b7
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-06-08 10:50:45 +0900

    Add files via upload

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -7,6 +7,8 @@ float dataf = 0;
 
 namespace CTRPluginFramework {
 void ludora(MenuEntry* entry) {
+  int oneunce;
+  if (oneunce == 1) {
   const Screen &top_screen = OSD::GetTopScreen();
   const Screen &top_screen2 = OSD::GetTopScreen();
   std::string logo = "Black Out";
@@ -15,15 +17,62 @@ void ludora(MenuEntry* entry) {
   u32 y = 1;
   u32 x2 = 1;
   u32 y2 = 21;
-  u32 sus = 1;
+  int slct;
   Color foreground = Color::SkyBlue;
   Color background = Color::Black;
   top_screen.Draw(logo, x, y, foreground, background);
   top_screen2.Draw(logo2, x2, y2, foreground, background);
-  while(sus==1) {
-  	if(Controller::IsKeysPressed(DPadUp)) {
-        logo2 = "1";
+  oneunce = 1;
+  }
+  	if(Controller::IsKeysPressed(L) && opned=0) {
+  	  slct = 0;
+      logo2 = "World";
       top_screen2.Draw(logo2, x2, y2, foreground, background);
+  	}else{
+  	  logo2 = "ver 0.1";
+  	  top_screen2.Draw(logo2, x2, y2, foreground, background);
+  	}
+  	if(Controller::IsKeysPressed(Right)) {
+  	  slct++;
+  	  if(slct ==-1) {
+  	    slct = 4;
+  	  }
+  	  if(slct == 4) {
+  	    slct =0;
+  	  }
+  	  if(slct == 0){
+  	    logo2="World";
+  	  }
+  	  if(slct == 1){
+  	    logo2="Combat";
+  	  }
+  	  if(slct == 2){
+  	    logo2="Player";
+  	  }
+  	  if(slct == 3){
+  	    logo2="Movement";
+  	  }
+  	}
+  	if(Controller::IsKeysPressed(Left)) {
+  	  slct += -1;
+  	  if(slct ==-1) {
+  	    slct = 4;
+  	  }
+  	  if(slct == 4) {
+  	    slct =0;
+  	  }
+  	  if(slct == 0){
+  	    logo2="World";
+  	  }
+  	  if(slct == 1){
+  	    logo2="Combat";
+  	  }
+  	  if(slct == 2){
+  	    logo2="Player";
+  	  }
+  	  if(slct == 3){
+  	    logo2="Movement";
+  	  }
   	}
   }
 }
