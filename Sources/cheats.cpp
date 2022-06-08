904468a5f1bf77c05c83ecf32135416c94d47579
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-06-08 14:18:30 +0900

    Add files via upload

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -8,79 +8,36 @@ float dataf = 0;
 namespace CTRPluginFramework {
 void ludora(MenuEntry* entry) {
   const Screen &top_screen = OSD::GetTopScreen();
-  const Screen &top_screen2 = OSD::GetTopScreen();
   std::string logo = "Black Out";
-  std::string logo2;
   u32 x = 1;
   u32 y = 1;
   u32 x2 = 1;
   u32 y2 = 21;
-  int slct;
-  int loop;
-  int opned;
+  int whilend = 0;
+  int slct = 0;
+  int indian;
   Color foreground = Color::SkyBlue;
   Color background = Color::Black;
   top_screen.Draw(logo, x, y, foreground, background);
-  
-  while (loop==0) {
-  if(Controller::IsKeysPressed(L)) {
-    opned++;
-    if (opned ==2) {
-     opned =0; 
+  while (whilend == 0) {
+    if(Controller::IsKeysPressed(L)) {
+      if (indian==0) {
+      indian = 1;
+      }else{
+        indian=0;
+      }
     }
-  }
-  if(opned==1) {
-  	slct = 0;
-    logo2 = "World";
-    top_screen2.Draw(logo2, x2, y2, foreground, background);
-  }
-  if(opned == 0);
+    }
+    if(Controller::IsKeysPressed(R)) {
+      whilend = 1;
+    }
+    if(Controller::IsKeysPressed(Right)) {
+      slct++;
+    }
+    if(Controller::IsKeysPressed(Left)) {
+      slct += -1
+    }
   
   }
-  if((Controller::IsKeysPressed(Right)) && (opned == 1)) {
-  	slct++;
-    if(slct ==-1) {
-    slct = 4;
-  }
-  	if(slct == 4) {
-  	  slct =0;
-  	  }
-  	  if(slct == 0){
-  	    logo2="World";
-  	  }
-  	if(slct == 1){
-  	  logo2="Combat";
-  	  }
-  	  if(slct == 2){
-  	    logo2="Player";
-  	  }
-  	  if(slct == 3){
-  	    logo2="Movement";
-  	  }
-  	  top_screen2.Draw(logo2, x2, y2, foreground, background);
-  	}
-  if((Controller::IsKeysPressed(Left)) && (opned == 1)) {
-  	  slct += -1;
-  	  if(slct ==-1) {
-  	    slct = 4;
-  	  }
-  	  if(slct == 4) {
-  	    slct =0;
-  	  }
-  	  if(slct == 0){
-  	    logo2="World";
-  	  }
-  	  if(slct == 1){
-  	    logo2="Combat";
-  	  }
-  	  if(slct == 2){
-  	    logo2="Player";
-  	  }
-  	  if(slct == 3){
-  	    logo2="Movement";
-  	  }
-  	  top_screen2.Draw(logo2, x2, y2, foreground, background);
-  	}
-
-}
+ }
 }  // namespace CTRPluginFramework
