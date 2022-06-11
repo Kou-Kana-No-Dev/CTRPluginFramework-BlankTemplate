b95987f7a983f130e39f2ade9996363e964a6956
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-06-11 15:03:02 +0900

    Add files via upload

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -7,10 +7,10 @@ float dataf = 0;
 
 namespace CTRPluginFramework {
 void ludora(MenuEntry* entry) {
-  u16 english;
-  u16 slct = 0;
-  Process::Read16(0x350B48 , english);
-  Process::Read16(0x350B4A , slct);
+  u16 englih;
+  u16 slt = 0;
+  Process::Read16(0x350B48 , englih);
+  Process::Read16(0x350B4A , slt);
   const Screen &top_screen = OSD::GetTopScreen();
   const Screen &top_screen2 = OSD::GetTopScreen();
   std::string logo = "Black Out";
@@ -26,34 +26,34 @@ void ludora(MenuEntry* entry) {
   Color background = Color::Black;
   top_screen.Draw(logo, x, y, foreground, background);
   if(Controller::IsKeysPressed(L)) {
-   if (english == 1) {
+   if (englih == 1) {
     Process::Write16(0x350B48 , 0x0000);
    }else{
     Process::Write16(0x350B48 , 0x0001);
    }
   }
   if(Controller::IsKeysPressed(Right)) {
-    if (english == 1) {
-      Process::Write16(0x350B4A , slct += 1);
-      Process::Read16(0x350B4A , slct);
-      if((slct == 0) && (english == 1)) {
+    if (englih == 1) {
+      Process::Write16(0x350B4A , slt += 1);
+      Process::Read16(0x350B4A , slt);
+      if((slt == 0) && (englih == 1)) {
         logo2 = "World";
         top_screen2.Draw(logo2, x2, y2, foreground, background);
       }else{
-      if((slct == 1) && (english == 1)) {
+      if((slt == 1) && (englih == 1)) {
         logo2 = "combat";
         top_screen2.Draw(logo2, x2, y2, foreground, background);
       }else{
-      if((slct == 2) && (english == 1)) {
+      if((slt == 2) && (englih == 1)) {
         logo2 = "Player";
         top_screen2.Draw(logo2, x2, y2, foreground, background);
       }else{
-        if((slct == 3) && (english == 1)) {
+        if((slt == 3) && (englih == 1)) {
         logo2 = "Movement";
         top_screen2.Draw(logo2, x2, y2, foreground, background);
       }else{
         Process::Write16(0x350B4A , 0x0000);
-        Process::Read16(0x350B4A , slct);
+        Process::Read16(0x350B4A , slt);
       }
     }
   }
@@ -61,27 +61,27 @@ void ludora(MenuEntry* entry) {
     }
   }
   if(Controller::IsKeysPressed(Left)) {
-    if (english == 1) {
-      Process::Write16(0x350B4A , slct += -1);
-      Process::Read16(0x350B4A , slct);
-      if((slct == 0) && (english == 1)) {
+    if (englih == 1) {
+      Process::Write16(0x350B4A , slt += -1);
+      Process::Read16(0x350B4A , slt);
+      if((slt == 0) && (englih == 1)) {
         logo2 = "World";
         top_screen2.Draw(logo2, x2, y2, foreground, background);
       }else{
-      if((slct == 1) && (english == 1)) {
+      if((slt == 1) && (englih == 1)) {
         logo2 = "combat";
         top_screen2.Draw(logo2, x2, y2, foreground, background);
       }else{
-      if((slct == 2) && (english == 1)) {
+      if((slt == 2) && (englih == 1)) {
         logo2 = "Player";
         top_screen2.Draw(logo2, x2, y2, foreground, background);
       }else{
-        if((slct == 3) && (english == 1)) {
+        if((slt == 3) && (englih == 1)) {
         logo2 = "Movement";
         top_screen2.Draw(logo2, x2, y2, foreground, background);
       }else{
         Process::Write16(0x350B4A , 0x0000);
-        Process::Read16(0x350B4A , slct);
+        Process::Read16(0x350B4A , slt);
       }
     }
   }
