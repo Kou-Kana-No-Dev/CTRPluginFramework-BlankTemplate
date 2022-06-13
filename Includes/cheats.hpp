c73d950ffb44863b763e84060059473304020436
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-06-13 18:16:34 +0900

    Add files via upload

--- Includes/cheats.hpp
+++ Includes/cheats.hpp
@@ -1,11 +1,12 @@
 #ifndef CHEATS_H
 #define CHEATS_H
 
+#include <CTRPluginFramework.hpp>
 #include "Helpers.hpp"
 #include "Unicode.h"
-#include <CTRPluginFramework.hpp>
 
-namespace CTRPluginFramework {
-void ludora(MenuEntry*);
-} // namespace CTRPluginFramework
+namespace CTRPluginFramework
+{
+   void E_field(MenuEntry* entry);
+}
 #endif
