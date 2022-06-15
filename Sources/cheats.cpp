c38b1ed2bc5148363cf04d9ea9d2647951f837cd
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-06-15 14:37:55 +0900

    Update cheats.cpp

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -150,7 +150,7 @@ void keykeysokesc(MenuEntry* entry) {
 void ATCK(MenuEntry* entry) {
   const Screen &OTCLI = OSD::GetTopScreen();
   u32 x3 = 5;
-  u32 y3 = 180;
+  u32 y3 = 230;
   Color foreground = Color::SkyBlue;
   Color background = Color::Black;
   std::string OTKR = "Auto Clicker : Enable";
