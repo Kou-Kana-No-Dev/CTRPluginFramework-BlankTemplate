71b1047aa642148c562b8ef8a4dc596763a589df
Kou Kana(No Dev) <104350461+Kou-Kana-No-Dev@users.noreply.github.com>
2022-06-17 23:20:55 +0900

    Add files via upload

---
+++ Sources/cheats.hpp
@@ -0,0 +1,15 @@
+#ifndef CHEATS_H
+#define CHEATS_H
+
+#include <CTRPluginFramework.hpp>
+#include "Helpers.hpp"
+#include "Unicode.h"
+
+namespace CTRPluginFramework
+{
+  void KeyStrokesButtom(MenuEntry* entry);
+  void KeyStrokesPad(MenuEntry* entry);
+  void KeyStrokesDPad(MenuEntry* entry);
+  void Test(MenuEntry* entry);
+}
+#endif
